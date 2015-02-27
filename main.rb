11316fa3e07476187de1876002ddf73bbfe7214e
Yasumasa Note <notex@mac.com>
2015-02-27 14:06:46 +0000

    modify sinatra view

--- main.rb
+++ main.rb
@@ -1,7 +1,26 @@
 require 'sinatra'
+require 'yaml'
+require 'pp'
+
+require './models'
 
 set :public_folder, File.dirname(__FILE__) + '/public'
 
+configure do
+    yml = YAML.load_file('settings.yml')
+    yml.each do |k, v|
+       set k, v
+    end
+end
+
 get '/' do
-  erb :index
+    @feeds = []
+    settings.rss_feeds.keys.each do |feed_key|
+        rss_feed_model = RssFeed.where(feed_key: feed_key).first
+        rss_entry_models = RssEntry.where(feed_key: feed_key).last
+        latest_rss_entry_model = RssEntry.where(feed_key: feed_key).last
+        @feeds << { :feed => rss_feed_model, :latest_entry => latest_rss_entry_model }
+    end
+    
+    erb :index
 end
